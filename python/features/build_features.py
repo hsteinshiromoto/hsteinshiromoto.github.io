998d7626d632e885559302e596cf588bf1777606
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2022-10-31 09:14:37 +1100

    fix bug with import of transformers

--- python/features/build_features.py
+++ python/features/build_features.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 sys.path.append(str(PROJECT_ROOT))
 
-import src.features.transformers as tf
+import python.features.transformers as tf
 
 
 def make_front_page(
